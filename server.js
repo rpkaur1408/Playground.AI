967bb2df279452faf66c72bd057a1258fef24083
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2024-06-03 13:42:14 -0400

    adding new files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express()
+const PORT = 8080
+
+app.get("/",(reg,res)=>{
+    res.send("hello");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
